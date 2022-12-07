c46e151719f6dca0b7e04d8e3950c166d74185f2
Vender102 <defender260613@gmail.com>
2022-12-07 11:22:46 +0500

    5 коммит

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def main():
     print("Hello world!")
-    print("Hello FMiIT!")
+    print("Hello FMiIT!!!")
     print("Номер моего коммита")
     print("9a76e70d134423dd5dcbf1d3174ea82e5540bb86")
 if __name__ == '__main__':
